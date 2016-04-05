c2fb50f9a0dc9cde719777e02076400d6d2211b2
Dave Fischoff <dave@davefischoff.com>
2016-04-05 12:54:31 -0400

    initial page load animations fixes #5

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -7,34 +7,34 @@ $(function () { // wait for document ready
 		controller = new ScrollMagic.Controller();
 
 		// define movement of panels
-		wipeAnimation = new TimelineMax();
-
-    $('.slide').slice(1).each(function(index, slide) {
-      console.log('sections',slide );
-      wipeAnimation.fromTo(slide, 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})  // in from left
-    })
-
-
-
-
-    var height = ($('.slide').length-1) * 100;
-		// create scene to pin and link animation
-		new ScrollMagic.Scene({
-				triggerElement: "#slides",
-				triggerHook: "onLeave",
-				duration: height+"%"
-			})
-			.setPin("#slides")
-      .on ('enter',function() {
-        $('#section-name').hide();
-
-      })
-      .on('leave',function() {
-        $('#section-name').show();
-      })
-			.setTween(wipeAnimation)
-			//.addIndicators() // add indicators (requires plugin)
-			.addTo(controller);
+		// wipeAnimation = new TimelineMax();
+    //
+    // $('.slide').slice(1).each(function(index, slide) {
+    //   console.log('sections',slide );
+    //   wipeAnimation.fromTo(slide, 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})  // in from left
+    // })
+    //
+    //
+    //
+    //
+    // var height = ($('.slide').length-1) * 100;
+		// // create scene to pin and link animation
+		// new ScrollMagic.Scene({
+		// 		triggerElement: "#slides",
+		// 		triggerHook: "onLeave",
+		// 		duration: height+"%"
+		// 	})
+		// 	.setPin("#slides")
+    //   .on ('enter',function() {
+    //     $('#section-name').hide();
+    //
+    //   })
+    //   .on('leave',function() {
+    //     $('#section-name').show();
+    //   })
+		// 	.setTween(wipeAnimation)
+		// 	//.addIndicators() // add indicators (requires plugin)
+		// 	.addTo(controller);
 
 
     new ScrollMagic.Scene({
